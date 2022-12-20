c203e0440e72d5aefff7802c500a74ecdbc41c4b
Jason Hegyessy <jason@hegyessy.com>
2022-12-20 10:26:59 -0800

    Update fresh

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,11 +1,6 @@
-/** @jsx h */
-import { h } from "preact";
-import { tw } from "@twind";
-
 export default function Home() {
-
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+    <div class="p-4 mx-auto max-w-screen-md">
       <h1>Hegyessy.com</h1>
       <p>One day this will be a website</p>
     </div>
--- utils/HandleSignIn.ts
+++
@@ -1,3 +0,0 @@
-export default function HandleSignIn () {
-  return console.log("clicked");
-}
